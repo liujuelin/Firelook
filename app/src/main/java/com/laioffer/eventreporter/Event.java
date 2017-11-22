c4caa5653d865c1ef696004371b9d7a5c1aa05ac
liujuelin <liu-juelin@outlook.com>
2017-11-21 19:44:57 -0800

    first commit

---
+++ app/src/main/java/com/laioffer/eventreporter/Event.java
@@ -0,0 +1,90 @@
+package com.laioffer.eventreporter;
+
+/**
+ * Created by Lau on 11/19/17.
+ */
+
+public class Event {
+    private int like;
+    private String id;
+    private long time;
+    private String username;
+    private String imgUri;
+    public Event() {}
+
+    public int getLike() {
+        return like;
+    }
+
+    public void setLike(int like) {
+        this.like = like;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public long getTime() {
+        return time;
+    }
+
+    public void setTime(long time) {
+        this.time = time;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getImgUri() {
+        return imgUri;
+    }
+
+    public void setImgUri(String imgUri) {
+        this.imgUri = imgUri;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    /**
+     * All data for a event.
+     */
+    private String title;
+    private String address;
+    private String description;
+
+    /**
+     * Constructor
+     */
+    public Event(String title, String address, String description) {
+        this.title = title;
+        this.address = address;
+        this.description = description;
+    }
+
+    /**
+     * Getters for private attributes of Event class.
+     */
+    public String getTitle() { return this.title; }
+    public String getAddress() { return this.address; }
+    public String getDescription() { return this.description; }
+
+}
---
+++ app/src/main/java/com/laioffer/eventreporter/EventReportActivity.java
@@ -0,0 +1,163 @@
+package com.laioffer.eventreporter;
+
+import android.app.Activity;
+import android.os.AsyncTask;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class EventReportActivity extends AppCompatActivity {
+    private static final String TAG = EventReportActivity.class.getSimpleName();
+    private EditText mEditTextLocation;
+    private EditText mEditTextTitle;
+    private EditText mEditTextContent;
+    private ImageView mImageViewSend;
+    private ImageView mImageViewCamera;
+    private DatabaseReference database;
+    private LocationTracker mLocationTracker;
+    private Activity mActivity;
+
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_event_report);
+
+        mEditTextLocation = (EditText) findViewById(R.id.edit_text_event_location);
+        mEditTextTitle = (EditText) findViewById(R.id.edit_text_event_title);
+        mEditTextContent = (EditText) findViewById(R.id.edit_text_event_content);
+        mImageViewCamera = (ImageView) findViewById(R.id.img_event_camera);
+        mImageViewSend = (ImageView) findViewById(R.id.img_event_report);
+        database = FirebaseDatabase.getInstance().getReference();
+
+        mImageViewSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String key = uploadEvent();
+            }
+        });
+
+        // check if GPS enabled
+        mActivity = this;
+        mLocationTracker = new LocationTracker(mActivity);
+        mLocationTracker.getLocation();
+        final double latitude = mLocationTracker.getLatitude();
+        final double longitude = mLocationTracker.getLongitude();
+
+        new AsyncTask<Void, Void, Void>() {
+            private List<String> mAddressList = new ArrayList<String>();
+
+            @Override
+            protected Void doInBackground(Void... urls) {
+                mAddressList = mLocationTracker.getCurrentLocationViaJSON(latitude,longitude);
+                return null;
+            }
+
+            @Override
+            protected void onPostExecute(Void input) {
+                if (mAddressList.size() >= 3) {
+                    mEditTextLocation.setText(mAddressList.get(0) + ", " + mAddressList.get(1) +
+                            ", " + mAddressList.get(2) + ", " + mAddressList.get(3));
+                }
+            }
+        }.execute();
+
+
+//auth
+        mAuth = FirebaseAuth.getInstance();
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
+                } else {
+                    Log.d(TAG, "onAuthStateChanged:signed_out");
+                }
+            }
+        };
+
+        mAuth.signInAnonymously().addOnCompleteListener(this,  new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                Log.d(TAG, "signInAnonymously:onComplete:" + task.isSuccessful());
+                if (!task.isSuccessful()) {
+                    Log.w(TAG, "signInAnonymously", task.getException());
+                }
+            }
+        });
+
+
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
+    }
+
+    private String uploadEvent() {
+        String title = mEditTextTitle.getText().toString();
+        String location = mEditTextLocation.getText().toString();
+        String description = mEditTextContent.getText().toString();
+        if (location.equals("") || description.equals("") ||
+                title.equals("") || Utils.username == null) {
+            return null;
+        }
+        //create event instance
+        Event event = new Event();
+        event.setTitle(title);
+        event.setAddress(location);
+        event.setDescription(description);
+        event.setTime(System.currentTimeMillis());
+        event.setUsername(Utils.username);
+        String key = database.child("events").push().getKey();
+        event.setId(key);
+        database.child("events").child(key).setValue(event, new DatabaseReference.CompletionListener() {
+            @Override
+            public void onComplete(DatabaseError databaseError, DatabaseReference databaseReference) {
+                if (databaseError != null) {
+                    Toast toast = Toast.makeText(getBaseContext(),
+                            "The event is failed, please check you network status.", Toast.LENGTH_SHORT);
+                    toast.show();
+                } else {
+                    Toast toast = Toast.makeText(getBaseContext(), "The event is reported", Toast.LENGTH_SHORT);
+                    toast.show();
+                    mEditTextTitle.setText("");
+                    mEditTextLocation.setText("");
+                    mEditTextContent.setText("");
+                }
+            }
+        });
+        return key;
+    }
+
+}
